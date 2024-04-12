3cd84eb9d22e8ec8273451100db66a6e84838674
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-12 00:51:46 -0700

    Web servrr

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,21 @@
-# Puppet manifest to configure Nginx
+# Puppet script to install and configure Nginx server with a root page containing "Hello World!" and a 301 redirect from /redirect_me to root.
 
 # Install Nginx package
 package { 'nginx':
     ensure => installed,
 }
 
-# Ensure Nginx service is running and enabled
+# Ensure the Nginx service is running and enabled to start on boot
 service { 'nginx':
     ensure     => running,
     enable     => true,
     require    => Package['nginx'],
 }
 
-# Create the default Nginx configuration file with a 301 redirect and Hello World page
+# Define the default Nginx configuration file with a 301 redirect from /redirect_me to root and a root page containing "Hello World!"
 file { '/etc/nginx/sites-available/default':
     ensure  => file,
-    content => epp('nginx_config/default.epp'), # This uses an Embedded Puppet template
+    content => template('nginx_config/default.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
@@ -23,35 +23,29 @@ file { '/etc/nginx/sites-available/default':
     notify  => Service['nginx'],
 }
 
-# Create an Embedded Puppet (EPP) template for the default Nginx config
-# Save this as /etc/puppetlabs/puppet/modules/nginx_config/templates/default.epp
-# <%#
+# Create the template file for the Nginx configuration (located in /etc/puppetlabs/puppet/modules/nginx_config/templates/default.erb)
+# This is the content of the template file:
+# <%# Nginx configuration template %>
 # server {
 #     listen 80 default_server;
+#     server_name _;
 #     root /var/www/html;
-#     index index.html index.htm index.nginx-debian.html;
-
+#     index index.html index.htm;
+#
 #     location / {
 #         try_files $uri $uri/ =404;
-#     }
-
-#     location /redirect_me {
-#         return 301 /;
-#     }
-    
-#     location / {
 #         default_type "text/html";
 #         return 200 "Hello World!\n";
 #     }
-
-#     error_page 404 /404.html;
-#     location = /404.html {
-#         root /usr/share/nginx/html;
+#
+#     location /redirect_me {
+#         return 301 /;
 #     }
+#
+#     error_page 404 /404.html;
 # }
-# %>
 
-# Ensure the index file contains "Hello World!"
+# Ensure the index file contains the text "Hello World!"
 file { '/var/www/html/index.html':
     ensure  => file,
     content => 'Hello World!\n',
@@ -61,4 +55,5 @@ file { '/var/www/html/index.html':
     require => Package['nginx'],
     notify  => Service['nginx'],
 }
+
 
